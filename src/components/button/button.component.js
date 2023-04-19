f8be54f8ca1f91033a43b3fe78b8c4f530b632f8
Anderson Melo <anderson@stalo.digital>
2023-04-19 17:06:53 +0100

    first commit

---
+++ src/components/button/button.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function ReactButton(props) {
+  return (
+    <button className="reactButton" type="button">
+      <span>React button</span>
+    </button>
+  );
+}
---
+++ src/root.component.js
@@ -0,0 +1,11 @@
+import ReactButton from "./components/button/button.component";
+
+export default function Root(props) {
+  return (
+    <div>
+      <h2>React App</h2>
+
+      <ReactButton />
+    </div>
+  );
+}
