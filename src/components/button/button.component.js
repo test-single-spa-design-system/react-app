799b6139169c3b603c27f9d0b2e980b32c88bb71
Anderson Melo <anderson@stalo.digital>
2023-04-20 15:52:55 +0100

    Configure the styleguide utility

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import { colors } from "@spms/styleguide";
+
 import "./styles.css";
 
 export default function ReactButton(props) {
   return (
-    <button className="reactButton" type="button">
-      <span>React button</span>
+    <button
+      className="reactButton"
+      type="button"
+      style={{ background: colors.primary }}
+    >
+      <span style={{ color: colors.secundary }}>React button</span>
     </button>
   );
 }
